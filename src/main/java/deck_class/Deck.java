2a4846a3300f2897f615ee1f807342b0cfd67ba4
haruna-ota <h-ota@biglobe.co.jp>
2018-08-07 11:39:27 +0900

    テストを書きました&toStringを作りました

--- src/main/java/deck_class/Deck.java
+++ src/main/java/deck_class/Deck.java
@@ -5,21 +5,30 @@ import card_class.Card;
 import java.util.List;
 
 public class Deck {    //山札のクラス
-    private List<Card> deck;   //山札
+    private List<Card> cards;   //山札
+    private int remainingCards; //山札の残りのカードの枚数
 
     //コンストラクタ
-    public Deck(List<Card> deck) {
-        this.deck = deck;
+    public Deck(List<Card> cards) {
+        this.cards = cards;
+        this.remainingCards = cards.size(); //山札のカードの枚数(初期値:52枚)
     }
 
-    //getter
-    public List<Card> getDeck() {
-        return deck;
+    //toString
+    @Override
+    public String toString() {
+        return cards.toString();
+    }
+
+    //カードの残り枚数を確認
+    public int getRemainingCards() {
+        return remainingCards;
     }
 
     //カードを1枚取り出す
     public Card drawACard() {
-        Card result = deck.get(0);
+        Card result = cards.get(cards.size() - remainingCards);     //52枚から山札の残りのカード枚数を引いたものが○枚目
+        remainingCards--;   //1枚引いたら、山札の残りのカードの枚数を1枚減らす
         return result;
     }
 }
--- src/test/java/deck_class/DeckTest.java
+++ src/test/java/deck_class/DeckTest.java
@@ -5,6 +5,8 @@ import card_class.CardRankEnum;
 import card_class.CardSuitEnum;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,7 +15,7 @@ import java.util.List;
 public class DeckTest {
     //テスト
     @Test
-    public void Stock() {
+    public void Deck() {
         List<Card> cardList = new ArrayList<>();    //空のリストを作成
         for (CardSuitEnum suit : CardSuitEnum.values()) {   //全てのマークとランクのカードをリストに入れる
             for (CardRankEnum rank : CardRankEnum.values()) {
@@ -23,7 +25,17 @@ public class DeckTest {
 
         Deck deck = new Deck(cardList);  //山札を生成
 
-        System.out.println(deck.getDeck());     //山札一覧
-        System.out.println(deck.drawACard());   //カードを一枚取り出す
+        //山札のカードの枚数を確認(52枚あればok)
+        assertSame(52, deck.getRemainingCards());
+
+        //山札のカードの1枚目を引く(HEART-ACE)
+        assertEquals("HEART-ACE", deck.drawACard().toString());
+
+        //山札のカードの2枚目を引く(HEART-2)
+        assertEquals("HEART-2", deck.drawACard().toString());
+
+        //山札のカードの枚数を確認(50枚あればok)
+        assertSame(50, deck.getRemainingCards());
+
     }
 }
