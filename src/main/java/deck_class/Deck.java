5d4d922a8e1967ae9636cee72f824b2639eeb462
haruna-ota <h-ota@biglobe.co.jp>
2018-08-07 13:45:41 +0900

    ご指摘いただいたところを修正しました

--- src/main/java/deck_class/Deck.java
+++ src/main/java/deck_class/Deck.java
@@ -6,12 +6,10 @@ import java.util.List;
 
 public class Deck {    //山札のクラス
     private List<Card> cards;   //山札
-    private int remainingCards; //山札の残りのカードの枚数
 
     //コンストラクタ
     public Deck(List<Card> cards) {
         this.cards = cards;
-        this.remainingCards = cards.size(); //山札のカードの枚数(初期値:52枚)
     }
 
     //toString
@@ -22,13 +20,13 @@ public class Deck {    //山札のクラス
 
     //カードの残り枚数を確認
     public int getRemainingCards() {
-        return remainingCards;
+        return cards.size();
     }
 
-    //カードを1枚取り出す
+    //山札のカードを1枚取り出す(先頭)
     public Card drawACard() {
-        Card result = cards.get(cards.size() - remainingCards);     //52枚から山札の残りのカード枚数を引いたものが○枚目
-        remainingCards--;   //1枚引いたら、山札の残りのカードの枚数を1枚減らす
+        Card result = cards.get(0);//山札の先頭のカードを1枚取り出す
+        cards.remove(0);   //山札の先頭のカードを1枚減らす(山札のカードの先頭が更新される)
         return result;
     }
 }
