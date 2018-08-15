d4eeee5696c737044ca1a46bfb73012b5db41f8f
haruna-ota <h-ota@biglobe.co.jp>
2018-08-15 11:22:07 +0900

    プレイヤーが自分でカードを引くかどうかを実装しました

--- src/main/java/judge/Player.java
+++ src/main/java/judge/Player.java
@@ -2,6 +2,8 @@ package judge;
 
 import card_class.Card;
 
+import java.util.Scanner;
+
 
 public class Player {
     private Hand handOfP; //プレイヤーの手札
@@ -23,7 +25,20 @@ public class Player {
 
     //山札からカードを引くかどうか答える
     public boolean isDrawingACard() {
-        return false;  //今回は必ず引かないと答える
+        System.out.println("プレイヤー：山札からカードを1枚引きますか？（y or nを入力して下さい）");
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {  //正しい入力が来るまでループする
+            String answerOfP = sc.next();   //プレイヤーが引く:yか、引かないか:nを標準入力で答える
+            if (answerOfP.equals("y")) {
+                return true;
+            } else if (answerOfP.equals("n")) {
+                return false;
+            } else {
+                System.out.println("正しく入力して下さい（y or n）");
+            }
+        }
     }
 
 }
